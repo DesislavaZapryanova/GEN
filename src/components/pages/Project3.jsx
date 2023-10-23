16a86dffe7099fbb422075b261bbb3cace5acfe0
D <desislava.zaprynova@gmail.com>
2023-10-24 00:21:06 +0300

    all projects code complete

--- src/components/pages/Project3.jsx
+++ src/components/pages/Project3.jsx
@@ -1,38 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Project.css';
 
 function Project3() {
+  const images = [
+    'images/bedroom1-1.jpg',
+    'images/living-room-3.jpg',
+    'images/living-room-1.jpg',
+    'images/bedroom1-3.jpg',
+    'images/bedroom1-2.jpg',
+    'images/living-room-2.jpg',
+  ];
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const openImage = (src, index) => {
+    setSelectedImage(src);
+    setCurrentIndex(index);
+  };
+
+  const closeImage = () => {
+    setSelectedImage(null);
+  };
+
+  const navigate = (direction) => {
+    const newIndex =
+      direction === 'prev'
+        ? (currentIndex - 1 + images.length) % images.length
+        : (currentIndex + 1) % images.length;
+
+    setSelectedImage(images[newIndex]); // Update the selected image
+    setCurrentIndex(newIndex);
+  };
   return (
-    <div>
-      <img
-        className="collection-image"
-        src="images/living-room-3.jpg"
-        alt="Living Room 3"
-      />
-      <img
-        className="collection-image"
-        src="images/living-room-2.jpg"
-        alt="Living Room 2"
-      />
-      <img
-        className="collection-image"
-        src="images/living-room-1.jpg"
-        alt="Living Room 1"
-      />
-      <img
-        className="collection-image"
-        src="images/bedroom1-1.jpg"
-        alt="Bedroom 1-1"
-      />
-      <img
-        className="collection-image"
-        src="images/bedroom1-2.jpg"
-        alt="Bedroom 1-2"
-      />
-      <img
-        className="collection-image"
-        src="images/bedroom1-3.jpg"
-        alt="Bedroom 1-3"
-      />
+    <div className="container-project-page">
+      <div className="container-images">
+        {images.map((src, index) => (
+          <img
+            key={index}
+            className="project1-image"
+            src={src}
+            alt={`Project ${index + 1}`}
+            onClick={() => openImage(src, index)}
+          />
+        ))}
+      </div>
+
+      {selectedImage && (
+        <div
+          className="image-modal"
+          onClick={closeImage}>
+          <button
+            className="arrow-button left-arrow"
+            onClick={(e) => {
+              e.stopPropagation(); // Stop propagation to prevent modal closure
+              navigate('prev');
+            }}>
+            &lt;
+          </button>
+          <img
+            src={selectedImage}
+            alt="Selected"
+            className="modal-image"
+          />
+          <button
+            className="arrow-button right-arrow"
+            onClick={(e) => {
+              e.stopPropagation(); // Stop propagation to prevent modal closure
+              navigate('next');
+            }}>
+            &gt;
+          </button>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/pages/Project4.jsx
+++ src/components/pages/Project4.jsx
@@ -1,18 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Project.css';
 
 function Project4() {
+  const images = ['images/bathroom-2.jpg', 'images/bathroom-1.jpg', 'images/bathroom-3.jpg'];
+
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const openImage = (src, index) => {
+    setSelectedImage(src);
+    setCurrentIndex(index);
+  };
+
+  const closeImage = () => {
+    setSelectedImage(null);
+  };
+
+  const navigate = (direction) => {
+    const newIndex =
+      direction === 'prev'
+        ? (currentIndex - 1 + images.length) % images.length
+        : (currentIndex + 1) % images.length;
+
+    setSelectedImage(images[newIndex]); // Update the selected image
+    setCurrentIndex(newIndex);
+  };
   return (
-    <div>
-      <img
-        className="collection-image"
-        src="images/2.jpg"
-        alt="Image 2"
-      />
-      <img
-        className="collection-image"
-        src="images/4.jpg"
-        alt="Image 4"
-      />
+    <div className="container-project-page">
+      <div className="container-images">
+        {images.map((src, index) => (
+          <img
+            key={index}
+            className="project1-image"
+            src={src}
+            alt={`Project ${index + 1}`}
+            onClick={() => openImage(src, index)}
+          />
+        ))}
+      </div>
+
+      {selectedImage && (
+        <div
+          className="image-modal"
+          onClick={closeImage}>
+          <button
+            className="arrow-button left-arrow"
+            onClick={(e) => {
+              e.stopPropagation(); // Stop propagation to prevent modal closure
+              navigate('prev');
+            }}>
+            &lt;
+          </button>
+          <img
+            src={selectedImage}
+            alt="Selected"
+            className="modal-image"
+          />
+          <button
+            className="arrow-button right-arrow"
+            onClick={(e) => {
+              e.stopPropagation(); // Stop propagation to prevent modal closure
+              navigate('next');
+            }}>
+            &gt;
+          </button>
+        </div>
+      )}
     </div>
   );
 }
