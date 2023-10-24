1a3f8d95cbea8bada62296db835bcdbd310e476f
D <desislava.zaprynova@gmail.com>
2023-10-24 19:03:14 +0300

    add contact page

--- src/components/pages/Contacts.jsx
+++ src/components/pages/Contacts.jsx
@@ -1,13 +1,94 @@
-import React from 'react';
-import Footer from '../Footer';
+// Contact.js
+
+import React, { useState } from 'react';
 import './Contacts.css';
 
-function Contacts() {
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setFormData({ name: '', email: '', message: '' });
+  };
+
   return (
-    <div className="container-contacts">
-      <Footer />
+    <div className="container-contacts-page">
+      <h1>CONTACT</h1>
+      <div className="container-forms">
+        <div className="container-form">
+          <form
+            id="contact-form"
+            onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="NAME"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+            <input
+              type="email"
+              placeholder="EMAIL"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+            <textarea
+              rows="10"
+              placeholder="MESSAGE"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required></textarea>
+            <button type="submit">
+              <div>
+                <i className="fa fa-paper-plane"></i>
+                <span>SEND</span>
+              </div>
+            </button>
+          </form>
+        </div>
+
+        <div className="container-form-social">
+          <ul>
+            <li>
+              <i className="fa fa-map-marker fa-2x" />
+              <span>City, State</span>
+            </li>
+            <li>
+              <i className="fa fa-phone fa-2x" />
+              <span>(212) 555-2368</span>
+            </li>
+            <li>
+              <i className="fa fa-envelope fa-2x" />
+              <span>
+                <a href="mailto:#">example@gmail.com</a>
+              </span>
+            </li>
+          </ul>
+          <hr />
+          <div className="social-icons">
+            <i className="fab fa-facebook-square" />
+            <i className="fab fa-instagram" />
+            <i className="fab fa-linkedin" />
+          </div>
+          <hr />
+          <div className="copyright">&copy; ALL OF THE RIGHTS RESERVED</div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default Contacts;
+export default Contact;
