1576a0f38c63858ecc7f24ead1205243343903b3
D <desislava.zaprynova@gmail.com>
2023-10-25 16:43:25 +0300

    about-page corrected location of the items

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -4,9 +4,9 @@ import './About.css';
 function About() {
   return (
     <div className="container-about-page">
-      <div className="container-texts">
-        <h1>Lorem ipsum</h1>
-        <div className="texts">
+      <div className="container-frame">
+        <div className="container-texts">
+          <h1>Lorem ipsum</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
@@ -17,13 +17,7 @@ function About() {
             voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
             inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
             ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
-            magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-            dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-            eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut
-            enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,
-            nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
-            ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum
-            fugiat quo voluptas nulla pariatur?"
+            magni dolores eos qui ratione voluptatem sequi nesciunt.
           </p>
         </div>
         <div className="image-container">
